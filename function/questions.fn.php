15f2aeb7da9062f9df560dbd4446c4e8bfdd01a5
Sonycodes <soniatavaresebirdll@gmail.com>
2024-02-28 21:32:20 +0100

    add:function(findAllQuestions)

--- function/questions.fn.php
+++ function/questions.fn.php
@@ -3,3 +3,17 @@ require_once '../config/conn.php';
 require_once 'database.fn.php';
 
 
+// Établir la connexion à la base de données
+   $conn = getPDOlink($config);
+
+//création fonction qui permet de récupérer les questions de la bdd pour la page ressouce
+
+function findAllQuestions($conn){
+    $sql= "SELECT * FROM `questions`";
+    $requete = $conn->query($sql);
+    $questions = $requete->fetchAll();
+    return $questions;
+}
+
+//pour visualiser le contenu du tableau qui est récupéré (attention echo ne pourra pas afficher le tableau)
+var_dump(findAllQuestions($conn));
