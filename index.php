60d60fee228e8e764860b44e789a4839e98ced30
Sonycodes <soniatavaresebirdll@gmail.com>
2024-03-07 22:06:14 +0100

    correction_position_avocate

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ include_once './utilities/nav_accueil.html';
     <div class="col-md-4 d-flex justify-content-md-end justify-content-center align-items-center"> 
       <img src="/assets/logo/logo_vierge.svg" class="img-fluid" alt="logo titre accueil" style="max-height: 180px; max-width: 250px">
     </div>
-    <div class="col-md-8">
+    <!-- il est en 6 sinon le avocate ne se met pas la ou je veux -->
+    <div class="col-md-6">
       <div class="container">
         <h1 class="display-1 text-center text-md-start text-duck">Jane Becker</h1>
         <h2 class="h1 text-center text-gray">Avocate</h2>  
