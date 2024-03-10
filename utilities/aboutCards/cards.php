8754f375832a6a8d5bc268fd85e8ffc94ddc4d4d
Ligia Aranda <arandaligia8@gmail.com>
2024-03-10 19:36:18 +0100

    arevoir

--- utilities/aboutCards/cards.php
+++ utilities/aboutCards/cards.php
@@ -1,12 +1,11 @@
-
 <?php
-            // Données
-            $competences = array(
-                array("Droit du travail", "fa-solid fa-briefcase", "Rupture abusive du contrat de travail <br> Heures de travail non payées<br>Harcèlement moral<br>Discrimination"),
-                array("Droit de la sécurité sociale", "fa-solid fa-notes-medical", "Refus de remboursement des frais médicaux<br>Contestation de l'évaluation du taux d'incapacité <br> Refus d'attribution d'une pension d'invalidité; <br> Contestation des droits aux prestations sociales"),
-                array("Droit de la famille ","fa-solid fa-people-roof", "Divorce par consentement mutuel; <br> Garde et droits de visite des enfants <br> Pension alimentaire pour les enfants ou le conjoint; <br> Succession et héritage"),
-                array("Expulsion locative","fa-solid fa-explosion", "Non-respect de la trêve hivernale;<br> Non-respect des conditions du bail de location par le propriétaire")
-            );
+// Données
+$competences = array(
+    array("Droit du travail", "fa-solid fa-briefcase", "Rupture abusive du contrat de travail <br> Heures de travail non payées<br>Harcèlement moral<br>Discrimination"),
+    array("Droit de la sécurité sociale", "fa-solid fa-notes-medical", "Refus de remboursement des frais médicaux<br>Contestation de l'évaluation du taux d'incapacité <br> Refus d'attribution d'une pension d'invalidité; <br> Contestation des droits aux prestations sociales"),
+    array("Droit de la famille ","fa-solid fa-people-roof", "Divorce par consentement mutuel; <br> Garde et droits de visite des enfants <br> Pension alimentaire pour les enfants ou le conjoint; <br> Succession et héritage"),
+    array("Expulsion locative","fa-solid fa-explosion", "Non-respect de la trêve hivernale;<br> Non-respect des conditions du bail de location par le propriétaire")
+);
 
 ?>
 <!-- cards compétences -->
@@ -44,6 +43,3 @@
         ?>
     </div>
 </div>
-
-
-
