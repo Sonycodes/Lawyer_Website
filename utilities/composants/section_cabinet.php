b766aec3bcf00b1d345d83d7eaac4630bddea171
Sonycodes <soniatavaresebirdll@gmail.com>
2024-03-14 16:38:15 +0100

    index_fix

--- utilities/composants/section_cabinet.php
+++ utilities/composants/section_cabinet.php
@@ -2,7 +2,8 @@
 <div class=" bg-container m-auto p-4 mt-5">
     <div class="d-flex flex-column justify-content-center align-items-center">
         <h1 class="text-duck text-center pb-5">Le Cabinet</h1>
-        <p class="h6 text-duck text-center fw-lighter">Le cabinet est situé au <strong class="fw-normal">127 rue de Rome</strong>, dans le 6e arrondissement de Marseille.<br> Facilement accessible en transport en commun <strong class="fw-normal">(tram T3, arrêt Rome-Dragon ; métro 1 et 2 Castellane)</strong>, celui-ci se trouve à proximité de la Préfecture, entre la Place Castellane et le Vieux-Port. Si vous souhaitez plus d’information n’hésitez pas à <strong class="fw-normal text-decoration-underline"> me contacter</strong>.</p>
+        <p class="h6 text-duck text-center fw-normal">Le cabinet est situé au <strong class="fw-bold">127 rue de Rome</strong>, dans le 6e arrondissement de Marseille.<br> Facilement accessible en transport en commun <strong class="">(tram T3, arrêt Rome-Dragon ; métro 1 et 2 Castellane)</strong>, celui-ci se trouve à proximité de la Préfecture, entre la Place Castellane et le Vieux-Port. Si vous souhaitez plus 
+        d’information n’hésitez pas à <a href="../../page_contact.php"><strong class="fw-bold text-decoration-underline"> me contacter</strong>.</p></a>
         <img src="/assets/photo_cabinet.png" alt="photo cabinet" class="img-fluid w-100 pt-3">
     </div>
 </div>
