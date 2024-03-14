ccad1e92122b5ee1ad9e5b72eaef2a72bccf2696
Sonycodes <soniatavaresebirdll@gmail.com>
2024-03-14 09:56:53 +0100

    error_page_add

---
+++ admin/error_page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- link Css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- link font awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+ <!-- link bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Error</title>
+
+    </head>
+<body>
+<div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold">404</h1>
+                <h2 class="h2"> <span class="text-danger">Opps!</span> Page non trouvé.</h2>
+                <h2 class="lead">
+                La page que vous cherchez n’est pas disponible.
+</h2>
+                <a href="../index.php" class="btn bg-duck p-3 rounded-5 fw-5 fs-5 shadow">Retourner à l’accueil</a>
+            </div>
+        </div>
+</body>
+</html>
